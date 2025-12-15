726624931af084829a21f504dd267de53d9548a3
Angelito Rubio <angelito.rubio@outlook.com>
2025-12-15 17:37:08 +0800

    update build settings for static export

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true, // Required for static export if you're using <Image />
-  },
-  basePath: '/your-repo-name', // 👈 important if not deploying to username.github.io
-  assetPrefix: '/your-repo-name/',
+  }
 };
 
 module.exports = nextConfig;
