7eb68a2bdd3e4d958fccacbde8a2f3b594958ea4
Angelito Rubio <angelito.rubio@outlook.com>
2025-12-15 18:07:12 +0800

    update basepath

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true, // Required for static export if you're using <Image />
-  }
+  },
+  basePath: '/Resume',
+  assetPrefix: '/Resume/',
 };
 
 module.exports = nextConfig;
